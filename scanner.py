1f2cea1203ec69f6528ae5ea64d11a9e77657681
Emirhan Cetinkaya <emirhan551952@gmail.com>
2024-06-29 13:58:07 +0300

    Branch mesajı

--- scanner.py
+++ scanner.py
@@ -1,7 +1,6 @@
 import os
 from google.cloud import vision
 import hashlib
-import shutil
 
 def hash_file(file_path):
     """Dosyanın hash değerini hesaplar."""
@@ -33,12 +32,14 @@ def rename_and_move_files():
 
     results = []
 
-    # 'readed' klasörünü oluştur
     readed_folder = 'readed'
     if not os.path.exists(readed_folder):
         os.makedirs(readed_folder)
+    
+    couldnt_read_folder = 'couldntreaded'
+    if not os.path.exists(couldnt_read_folder):
+        os.makedirs(couldnt_read_folder)
 
-    # 'kodlar' klasöründeki resim dosyalarını aç
     kodlar_folder = 'kodlar'
     image_files = [file for file in os.listdir(kodlar_folder) if file.endswith('.jpg') or file.endswith('.png')]
     for filename in image_files:
@@ -50,18 +51,15 @@ def rename_and_move_files():
         response = client.text_detection(image=image)
         texts = response.text_annotations
 
-        # Bitişik metinleri ekrana yazdır
         if texts:
             unique_texts = set()
             for text in texts:
-                # Boşluk içermeyen metinleri kontrol et
                 if ' ' not in text.description and 8 <= len(text.description) <= 12:
                     if text.description not in unique_texts:
                         unique_texts.add(text.description)
                         new_filename = f'{text.description}.jpg'
                         new_filepath = os.path.join(readed_folder, new_filename)
 
-                        # Dosya adı zaten varsa, yeni bir adla kaydet
                         if os.path.exists(new_filepath):
                             base, extension = os.path.splitext(new_filename)
                             counter = 1
@@ -70,13 +68,17 @@ def rename_and_move_files():
                                 new_filepath = os.path.join(readed_folder, new_filename)
                                 counter += 1
 
-                        print(f'{filename} | {text.description}')
-                        os.rename(file_path, new_filepath)
+                        try:
+                            os.rename(file_path, new_filepath)
+                            print(f'{filename} | {text.description}')
+                        except FileNotFoundError:
+                            print(f"Hata: {file_path} dosyası taşınamadı.")
                         results.append(text.description)
         else:
+            couldnt_read_path = os.path.join(couldnt_read_folder, filename)
+            os.rename(file_path, couldnt_read_path)
             print(f'{filename} | Metin bulunamadı.')
 
     print("\nToplu olarak kodlar:\n")
     for result in results:
         print(result)
-
